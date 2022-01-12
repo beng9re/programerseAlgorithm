ffe7e4d5fcb606fcc4fca4d365acdfd0426cb001
kbh0581 <kbh0581@gmail.com>
2022-01-12 17:20:25 +0900

    feat: Carpet 문제 해결

--- src/main/java/exhaustiveSearch/Carpet/Carpet.java
+++ src/main/java/exhaustiveSearch/Carpet/Carpet.java
@@ -1,9 +1,24 @@
 package exhaustiveSearch.Carpet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Carpet {
 
 	public int[] solution(int brown, int yellow) {
 		int[] answer = {};
+		int totalTile = brown + yellow;
+		for (int y = 1; y < brown + 1 ; y++) {
+			if (totalTile % y == 0) {
+				int x = totalTile / y;
+				int positionBlown = x * 2 + ((y - 2) * 2);
+				int positionYellow = totalTile - positionBlown;
+				if (positionBlown == brown && positionYellow == yellow) {
+					return new int[]{x,y};
+				}
+			}
+		}
+
 		return answer;
 
 
